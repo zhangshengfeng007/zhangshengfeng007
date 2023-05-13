bc0e5327161288040ebcae66b16ffbd6cf2ecbee
zhangshengfeng007 <1004393374@qq.com>
2023-05-13 14:54:53 +0800

    1. 将报警温度从46°C降低到45°C

--- ABF003_pro_max/Core/ZJ_Drivers/ZJ_Inc/NTC.h
+++ ABF003_pro_max/Core/ZJ_Drivers/ZJ_Inc/NTC.h
@@ -4,16 +4,16 @@
 
 #define NORMAL_TEMP 	40
 #define OVER_TEMP 		42
-#define STOP_TEMP 		46//44
+#define STOP_TEMP 		45//44
 
 #define TEMP_TAB_INDEX  15
 #define NTC_CHANNEL 1
 extern uint8_t ntc_adc_enable_flag;
-extern int8_t system_temp;	
+extern int8_t system_temp;
 extern uint8_t over_temp_flag;
 
 int8_t NTC_Temp_Check(void);
-extern void NTC_Temp_Running(void);	
+extern void NTC_Temp_Running(void);
 
 #endif
 
