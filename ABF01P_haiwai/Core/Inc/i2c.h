2f822e1e5a4a665d8ef0277bf1d13a0426acfc2d
zhangshengfeng007 <1004393374@qq.com>
2023-05-30 12:09:53 +0800

    1. 将IO口宏定义 从main.h中，转移到gpio.h中 2. 修改有用到宏定义GPIO,的.h文件

--- ABF01P_haiwai/Core/Inc/i2c.h
+++ ABF01P_haiwai/Core/Inc/i2c.h
@@ -27,7 +27,7 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "gpio.h"
 /* USER CODE BEGIN Includes */
 
 /* USER CODE END Includes */
