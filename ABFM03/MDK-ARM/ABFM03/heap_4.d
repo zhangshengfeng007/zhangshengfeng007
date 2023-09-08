abdc95790d3651de2fe52815f9b4f4aea97fbf4e
zhangshengfeng007 <1004393374@qq.com>
2023-09-08 18:28:26 +0800

    1. 将MDK文件夹备份上传，保证下次下载之后，编译没问题

---
+++ ABFM03/MDK-ARM/ABFM03/heap_4.d
@@ -0,0 +1,13 @@
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+.\abfm03\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+.\abfm03\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\abfm03\heap_4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\abfm03\heap_4.o: ../Core/Inc/FreeRTOSConfig.h
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM0/portmacro.h
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+.\abfm03\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ ABFM03/MDK-ARM/ABFM03/queue.d
@@ -0,0 +1,15 @@
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/queue.c
+.\abfm03\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\abfm03\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+.\abfm03\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\abfm03\queue.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\abfm03\queue.o: ../Core/Inc/FreeRTOSConfig.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM0/portmacro.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+.\abfm03\queue.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/queue.h
